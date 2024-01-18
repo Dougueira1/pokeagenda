c557e354c45968713e637abc14e4951a7a56ef0d
Douglas <douglas.jose.silva1@outlook.com>
2024-01-17 22:30:40 -0300

    FINALIZAMOS A CONFIG DO PROJETO

---
+++ src/app/_module/_core/page/core.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'core-page', 
+    templateUrl: './core.page.html',
+    styleUrl:'./core.page.scss',
+})
+export class CorePage{}
+
